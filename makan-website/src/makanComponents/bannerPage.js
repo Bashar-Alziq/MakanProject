2b18a7d62ac05b5a0ce4c33a6cc0d2cbafcea178
Bashar-Alziq <102527790+Bashar-Alziq@users.noreply.github.com>
2022-04-05 16:34:31 +0300

    updated

--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -5,7 +5,7 @@ const Banner = () =>
     
     return(
      <div>
-         <h1>Mr Bashar is the best in the west also your focken osta</h1>
+         <h1>Mr Bashar is the best in the west also your focken ostas</h1>
      <h3 className = "text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
      <nav>Alziq</nav>
      </div>
