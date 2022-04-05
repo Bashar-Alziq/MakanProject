8737bbfa75288579b0adbca57cfe1750de953118
AHD-02 <a.alziq2002@gmail.com>
2022-04-05 14:27:22 +0300

    updates

---
+++ makan-website/src/makanComponents/bannerPage.js
@@ -0,0 +1,13 @@
+
+
+const Banner = () => 
+{
+    
+    return(
+     <div>
+     <h3 className = "text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
+     <nav>Alziq</nav>
+     </div>
+    )
+}
+export default Banner
