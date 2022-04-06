9666d25cefad7d4c154c6b32bb1e09f9310f3578
Bashar-Alziq <102527790+Bashar-Alziq@users.noreply.github.com>
2022-04-06 15:16:16 +0300

    bashar

--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -5,10 +5,11 @@ const Banner = () =>
     
     return(
      <div>
-         <h1 className="text-center bg-slate-600 "> bashar alziq -tst txt </h1>
+         <h1 className="text-center bg-slate-600 "> bashar alziq -tst txt xxxx </h1>
      <h3 className = "bg-dark text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
      <nav>Alziq</nav>
      </div>
     )
 }
+
 export default Banner
