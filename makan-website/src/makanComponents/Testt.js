ffa17a00b081b53c8971a04df1bff1e3fc99cca9
Bashar-Alziq <102527790+Bashar-Alziq@users.noreply.github.com>
2022-04-06 14:47:56 +0300

    'bashar'

--- makan-website/src/makanComponents/Testt.js
+++ makan-website/src/makanComponents/Testt.js
@@ -3,7 +3,7 @@ import React from "react";
 const Testt = () => {
     return (
         <div>
-                
+           <p>Hello Ahmad</p>     
         </div>
     )
     
--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -5,7 +5,7 @@ const Banner = () =>
     
     return(
      <div>
-         <h1 className="text-center bg-slate-600 ">bashar alziq</h1>
+         <h1 className="text-center bg-slate-600 "> bashar alziq -tst txt </h1>
      <h3 className = "text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
      <nav>Alziq</nav>
      </div>
