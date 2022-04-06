fc493e480e67ae9a72016972395ea63e30c37405
Bashar-Alziq <102527790+Bashar-Alziq@users.noreply.github.com>
2022-04-06 14:59:04 +0300

    hello2

--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -6,7 +6,7 @@ const Banner = () =>
     return(
      <div>
          <h1 className="text-center bg-slate-600 "> bashar alziq -tst txt </h1>
-     <h3 className = "text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
+     <h3 className = "bg-dark text-blue-600 hover:text-green-600 pointer">Ahmad</h3>
      <nav>Alziq</nav>
      </div>
     )
